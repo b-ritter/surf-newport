ab8263e2433155b67f4884341fa9b6274ff2db00
b-ritter <ben@benjaminritter.com>
2016-01-21 14:34:18 -0800

    Populate list items, bind click handler

--- _js/newp_app.js
+++ _js/newp_app.js
@@ -2,6 +2,11 @@
 var map,
     locationList = ko.observableArray([]);
 
+var Loc = function(data){
+  this.locName = ko.observable(data.locName);
+  this.marker = ko.observable(data.marker);
+};
+
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 33.6417186, lng: -117.9143306},
@@ -10,15 +15,26 @@ function initMap() {
   });
 
   $.getJSON('_data/locations.json', function(data){
+    // Make markers for each data item
     data.forEach(function(locationItem){
-      locationList.push(locationItem);
       var marker = new google.maps.Marker({
         position: {lat: locationItem.location[0], lng: locationItem.location[1]},
         map: map,
-        title: locationItem.name
+        title: locationItem.locName
       });
+      locationItem.marker = marker;
+      locationList.push(new Loc(locationItem));
     });
-    
+
+    var ListViewModel = function(){
+      this.locations = locationList;
+      this.showLocation = function(){
+        console.log(this.marker());
+      };
+    };
+
+    ko.applyBindings(new ListViewModel(), document.getElementById('location-list'));
+
   }).error(
     function() {
       console.log('foo');
