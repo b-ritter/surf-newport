02d26219a4952dc292c32e4976a15a48df9e7cd6
b-ritter <ben@benjaminritter.com>
2016-02-01 07:48:53 -0800

    Make current list item text highlight on marker and list clicks.

--- js/newp_app.js
+++ js/newp_app.js
@@ -1,9 +1,6 @@
 // Hardcode the location data for the surf spots since
 // these don't really change much over time. The spot
 // id is required for the magicseaweed api.
-
-
-
 var locationData = [
   {
     "locName": "Newport Jetties",
@@ -31,54 +28,18 @@ var locationData = [
     "spotID": "2579",
     "location": [ 33.592816, -117.877136 ]
   }
-  /*
-  // Maybe include Huntington
-  ,{
-    "locName": "Huntington Pier",
-    "spotID": "286",
-    "location": [ 33.655574, -118.003023 ]
-  },
-  {
-    "locName": "Goldenwest",
-    "spotID": "4039",
-    "location": [ 33.667683, -118.018097 ]
-  },
-  {
-    "locName": "Bolsa Chica",
-    "spotID": "3797",
-    "location": [ 33.700753, -118.053186 ]
-  },
-  {
-    "locName": "Seal Beach Pier",
-    "spotID": "285",
-    "location": [ 33.700753, -118.053186 ]
-  }
-  */
 ];
-
 
 // Google map and surf location stuff
 var map,
-    ko,
-    $,
-    google,
-    oauthSignature,
-    console,
-    document,
-    _,
-    setTimeout,
     mapCenter = { lat: 33.623201, lng: -117.9312093},
     currentItem = null,
     locationList = ko.observableArray([]),
-    markerAnimationCycleLength = 2100;
-
-var currentItemDisplay = ko.observable(currentItem);
-
-function setCurrent() {
-  currentItemDisplay(currentItem);
-}
-// Yelp stuff
-var httpMethod = 'GET',
+    currentItemDisplay = ko.observable(currentItem),
+    categoryList = ko.observableArray([]),
+    markerAnimationCycleLength = 2100,
+    // Yelp credentials and parameters
+    httpMethod = 'GET',
     yelpUrl = 'https://api.yelp.com/v2/search?',
     parameters = {
         bounds: '33.587063, -117.968421|33.671254, -117.867103',
@@ -94,32 +55,42 @@ var httpMethod = 'GET',
     },
     consumerSecret = 'BAK4r2WoFYdc2nSoGrjD14pc7Bo',
     tokenSecret = 'qb99XpkZ-lV26f7eNQ1nVofsGN8',
-    // generates a RFC 3986 encoded, BASE64 encoded HMAC-SHA1 hash
+    // Create signature for the request
     signature = oauthSignature.generate(httpMethod, yelpUrl, parameters, consumerSecret, tokenSecret,
        { encodeSignature: false});
-
+    // Add the signature to the query string
     parameters.oauth_signature = signature;
 
-/* TODO: Create custom map binding to capture clicks
-ko.bindingHandlers.gMap = {
-  update: function(element, valueAccessor, allBindings, viewModel, bindingContext){
-
+// Utility function to set the state of the app
+function setCurrent(item) {
+  if(currentItem !== null && item !== null){
+    currentItem.isActive(false);
+    item.isActive(true);
+  }else if(currentItem !== null && item === null){
+    currentItem.isActive(false);
   }
-};
-*/
+  currentItem = item;
+  currentItemDisplay(item);
+}
 
 // Class definition for a surf location item
 // Hits the magicseaweed api from php
 var Loc = function(data){
   var self = this;
   this.locName = ko.observable(data.locName);
+  this.isActive = ko.observable(false);
   this.marker = ko.observable(data.marker);
   this.markerInfo = ko.observable(data.markerInfo);
   this.map = ko.observable(data.map);
   this.spotID = data.spotID || null;
   this.forecast = null;
+  this.businessInfo = ko.observable(data.businessInfo);
+  // this.phone = ko.observable(data.phone);
   this.marker().addListener('click',function(){
     self.openInfoWindow();
+  });
+  this.markerInfo().addListener('closeclick', function(){
+    setCurrent(null);
   });
   this.loadInfo = function(){
     var self = this;
@@ -143,6 +114,8 @@ var Loc = function(data){
     }
   };
 };
+
+// Define method on prototype to open info window for a marker
 Loc.prototype.openInfoWindow = function(){
   if(currentItem !== null){
     currentItem.markerInfo().close();
@@ -156,10 +129,14 @@ Loc.prototype.openInfoWindow = function(){
       self.marker().setAnimation(null);
     }, markerAnimationCycleLength);
   }
-  currentItem = this;
-  setCurrent();
+  // making currentItem itself an observable prevents the info window
+  // from opening
+// currentItem = this;
+  // setCurrent() sets an observable equal to the current item
+  setCurrent(this);
 };
-// Define the callback function
+
+// Define the callback function that kicks off the entire map
 function initMap() {
   // First, we need to make the map object
   // Markers need to reference this object
@@ -169,63 +146,27 @@ function initMap() {
     zoom: 13
   });
 
-  // Add Yelp stuff once the map is loaded
-  google.maps.event.addListenerOnce(map, 'idle', function(){
-      // When the map is loaded, add the other locations
-      $.ajax({
-        url: yelpUrl,
-        jsonpCallback: 'cb',
-        dataType: 'jsonp',
-        data: parameters,
-        cache: true,
-
-        success: function(data){
-          console.log(data);
-          data.businesses.forEach(function(business){
-            var marker = new google.maps.Marker({
-              position: {
-                lat: business.location.coordinate.latitude,
-                lng: business.location.coordinate.longitude},
-              map: map,
-              title: business.name
-            });
-            var markerInfo = new google.maps.InfoWindow(
-              {content: "<h2>"+business.name+"</h2>" }
-            );
-            locationList.push(new Loc({
-              marker: marker,
-              markerInfo: markerInfo,
-              map: map,
-              locName: business.name
-            }));
-          });
-        }
-      }).
-      fail(
-        function(){
-          // TODO: Give something back to Knockout
-        }
-      );
-  });
-
   // Make markers for each data item
-  // TODO: make markers observable
   locationData.forEach(function(locationItem){
+    // Create a marker
     var marker = new google.maps.Marker({
       position: {lat: locationItem.location[0], lng: locationItem.location[1]},
       map: map,
       title: locationItem.locName
     });
-
+    // Create a marker info window
     var markerInfo = new google.maps.InfoWindow(
       {content: "<h2>"+locationItem.locName+"</h2>" }
     );
+    // Assign the map marker and info window to the locationItem,
+    // which is passed to Loc() to make an object with observable properties
     locationItem.marker = marker;
     locationItem.markerInfo = markerInfo;
     locationItem.map = map;
     locationList.push(new Loc(locationItem));
   });
 
+  // Setup the ViewModel for the markers
   var ListViewModel = function(){
     var self = this;
     this.searchTerm = ko.observable('');
@@ -253,29 +194,69 @@ function initMap() {
       if(tempList.length === 0){
         // If there are no results that pass the filter,
         // show them all
+        setCurrent(null);
         return locationList();
       } else if (tempList.length === 1){
         // If there is only one match, set it as the current item
-        currentItem = tempList[0];
-        currentItem.openInfoWindow();
+        setCurrent(tempList[0]);
+        // currentItem.openInfoWindow();
         return currentItem;
       }else {
+        setCurrent(null);
         return tempList;
       }
     });
+
     this.showLocation = function(){
       this.openInfoWindow();
-      setDumdum();
+      setCurrent(this);
     };
   };
 
   ko.applyBindings(new ListViewModel(), document.getElementById('NewportMesaApp'));
+
+  // Add Yelp data once the map is loaded
+  google.maps.event.addListenerOnce(map, 'idle', function(){
+      // When the map is loaded, add the other locations
+      $.ajax({
+        url: yelpUrl,
+        jsonpCallback: 'cb',
+        dataType: 'jsonp',
+        data: parameters,
+        cache: true,
+        success: function(data){
+          console.log(data);
+          data.businesses.forEach(function(business){
+            var marker = new google.maps.Marker({
+              position: {
+                lat: business.location.coordinate.latitude,
+                lng: business.location.coordinate.longitude},
+              map: map,
+              title: business.name
+            });
+            var markerInfo = new google.maps.InfoWindow(
+              {content: "<h2>"+business.name+"</h2>" }
+            );
+            locationList.push(new Loc({
+              marker: marker,
+              markerInfo: markerInfo,
+              map: map,
+              locName: business.name,
+              businessInfo: {
+                phone: business.phone,
+                url: business.url,
+                snippet: business.snippet,
+                rating: business.rating,
+                review_count: business.review_count
+              }
+            }));
+          });
+        }
+      }).
+      fail(
+        function(){
+          // TODO: Give something back to Knockout
+        }
+      );
+  });
 }
-
-
-
-// var marker = new google.maps.Marker({
-//   position: { lat: locationItem.location[0], lng: locationItem.location[1] },
-//   map: map,
-//   title: locationItem.name
-// });
