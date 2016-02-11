7f07729b0e939f903ea1cd0448b156a0b1d74f46
b-ritter <ben@benjaminritter.com>
2016-02-11 09:35:52 -0800

    Surf filter button sort of works

--- js/newp_app.js
+++ js/newp_app.js
@@ -258,42 +258,42 @@ function initMap() {
     var self = this;
 
     this.locations = ko.computed(function(){
-      // Live filtering of locations
-      if(currentItem.locType !== 'default'){
-        currentItem.markerInfo().close();
-      }
-
-      var tempList = _.filter(locationList(), function(item){
-        var itemChars = item.locName().toLowerCase();
-        var searchTermChars = searchTerm().toLowerCase();
-        if(itemChars.indexOf(searchTermChars) !== -1){
-          // Check if the marker is NOT visible
-          if(!item.marker().getVisible()){
-            // If it wasn't visible, show it
-            item.marker().setVisible(true);
-          }
-          // Give the item back to tempList to show it
-          return item;
-        } else {
-          // Turn off marker if it fails the filter test
-          item.marker().setVisible(false);
+        // Live filtering of locations
+        if(currentItem.locType !== 'default'){
+          currentItem.markerInfo().close();
         }
-      });
 
-      if(tempList.length === 0){
-        // If there are no results that pass the filter,
-        // show them all
-        setCurrent(defaultLoc);
-        return locationList();
-      } else if (tempList.length === 1){
-        // If there is only one match, set it as the current item
-        setCurrent(tempList[0]);
-        currentItem.openInfoWindow();
-        return currentItem;
-      } else {
-        setCurrent(defaultLoc);
-        return tempList;
-      }
+        var tempList = _.filter(locationList(), function(item){
+          var itemChars = item.locName().toLowerCase();
+          var searchTermChars = searchTerm().toLowerCase();
+          if(itemChars.indexOf(searchTermChars) !== -1){
+            // Check if the marker is NOT visible
+            if(!item.marker().getVisible()){
+              // If it wasn't visible, show it
+              item.marker().setVisible(true);
+            }
+            // Give the item back to tempList to show it
+            return item;
+          } else {
+            // Turn off marker if it fails the filter test
+            item.marker().setVisible(false);
+          }
+        });
+
+        if(tempList.length === 0){
+          // If there are no results that pass the filter,
+          // show them all
+          setCurrent(defaultLoc);
+          return locationList();
+        } else if (tempList.length === 1){
+          // If there is only one match, set it as the current item
+          setCurrent(tempList[0]);
+          currentItem.openInfoWindow();
+          return currentItem;
+        } else {
+          setCurrent(defaultLoc);
+          return tempList;
+        }
     });
 
     this.showLocation = function(){
@@ -302,16 +302,20 @@ function initMap() {
     };
 
     this.showSurfSpots = function(){
-      var surfSpots = _.filter(locationList(), function(item){
-        if (item.locType === 'surf'){
-          return item;
-        }
-      });
-      // this.locations(surfSpots);
+      locationList(self.filterByType('surf'));
     };
 
     this.showRestaurants = function(){
+      // locationList(self.filterByType('biz'));
+    };
 
+    this.filterByType = function(type){
+      var locs = _.filter(locationList(), function(item){
+        if (item.locType === type){
+          return item;
+        }
+      });
+      return locs;
     };
   };
 
@@ -328,7 +332,7 @@ function initMap() {
         cache: true,
         success: function(data){
           data.businesses.forEach(function(business){
-            console.log(business);
+            // console.log(business);
             var categories = business.categories.reduce(function(previousValue, currentValue, currentIndex, array){
               var separator = "";
               if(currentIndex < array.length-1){
