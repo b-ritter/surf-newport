14d8c62fc35bd80aa5e98e79409666ef327d48d1
b-ritter <ben@benjaminritter.com>
2016-02-15 15:49:07 -0800

    Move the ViewModel definition out of the initMap callback

--- js/newp_app.js
+++ js/newp_app.js
@@ -198,6 +198,7 @@ ko.bindingHandlers.MSWswellChart = {
   }
 };
 
+// Custom location object types for Knockout
 var Loc = function(data){
   var self = this;
   this.locName = ko.observable(data.locName);
@@ -326,7 +327,74 @@ BizLoc = function(data){
 
 BizLoc.prototype = Object.create(Loc.prototype);
 
-// Define the callback function that kicks off the entire map
+// Setup the ViewModel for the markers
+var NewportViewModel = function(){
+  var self = this;
+
+  this.locations = ko.computed(function(){
+      // Live filtering of locations
+      if(currentItem.locType !== 'default'){
+        currentItem.markerInfo().close();
+      }
+
+      var tempList = _.filter(locationList(), function(item){
+        var itemChars = item.locName().toLowerCase();
+        var searchTermChars = searchTerm().toLowerCase();
+        if(itemChars.indexOf(searchTermChars) !== -1){
+          // Check if the marker is NOT visible
+          if(!item.marker().getVisible()){
+            // If it wasn't visible, show it
+            item.marker().setVisible(true);
+          }
+          // Give the item back to tempList to show it
+          return item;
+        } else {
+          // Turn off marker if it fails the filter test
+          item.marker().setVisible(false);
+        }
+      });
+
+      if(tempList.length === 0){
+        // If there are no results that pass the filter,
+        // show them all
+        setCurrent(defaultLoc);
+        return locationList();
+      } else if (tempList.length === 1){
+        // If there is only one match, set it as the current item
+        setCurrent(tempList[0]);
+        currentItem.openInfoWindow();
+        return currentItem;
+      } else {
+        setCurrent(defaultLoc);
+        return tempList;
+      }
+  });
+
+  this.showLocation = function(){
+    this.openInfoWindow();
+    setCurrent(this);
+  };
+
+  /* TODO: Remove or keep filter buttons */
+  this.showSurfSpots = function(){
+    locationList(self.filterByType('surf'));
+  };
+
+  this.showRestaurants = function(){
+    locationList(self.filterByType('biz'));
+  };
+
+  this.filterByType = function(type){
+    var locs = _.filter(locationList(), function(item){
+      if (item.locType === type){
+        return item;
+      }
+    });
+    return locs;
+  };
+};
+
+// Define the callback function that kicks off the entire app
 function initMap() {
   // First, we need to make the map object
   // Markers need to reference this object
@@ -363,73 +431,6 @@ function initMap() {
     locationItem.map = map;
     locationList.push(new SurfLoc(locationItem));
   });
-
-  // Setup the ViewModel for the markers
-  var NewportViewModel = function(){
-    var self = this;
-
-    this.locations = ko.computed(function(){
-        // Live filtering of locations
-        if(currentItem.locType !== 'default'){
-          currentItem.markerInfo().close();
-        }
-
-        var tempList = _.filter(locationList(), function(item){
-          var itemChars = item.locName().toLowerCase();
-          var searchTermChars = searchTerm().toLowerCase();
-          if(itemChars.indexOf(searchTermChars) !== -1){
-            // Check if the marker is NOT visible
-            if(!item.marker().getVisible()){
-              // If it wasn't visible, show it
-              item.marker().setVisible(true);
-            }
-            // Give the item back to tempList to show it
-            return item;
-          } else {
-            // Turn off marker if it fails the filter test
-            item.marker().setVisible(false);
-          }
-        });
-
-        if(tempList.length === 0){
-          // If there are no results that pass the filter,
-          // show them all
-          setCurrent(defaultLoc);
-          return locationList();
-        } else if (tempList.length === 1){
-          // If there is only one match, set it as the current item
-          setCurrent(tempList[0]);
-          currentItem.openInfoWindow();
-          return currentItem;
-        } else {
-          setCurrent(defaultLoc);
-          return tempList;
-        }
-    });
-
-    this.showLocation = function(){
-      this.openInfoWindow();
-      setCurrent(this);
-    };
-
-    /* TODO: Remove or keep filtering */
-    this.showSurfSpots = function(){
-      locationList(self.filterByType('surf'));
-    };
-
-    this.showRestaurants = function(){
-      locationList(self.filterByType('biz'));
-    };
-
-    this.filterByType = function(type){
-      var locs = _.filter(locationList(), function(item){
-        if (item.locType === type){
-          return item;
-        }
-      });
-      return locs;
-    };
-  };
 
   ko.applyBindings(new NewportViewModel(), document.getElementById('NewportMesaApp'));
 
